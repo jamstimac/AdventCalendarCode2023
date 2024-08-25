b805394f7714dc46f603cfe9de69a7aef0b2fe7a
chylveon <jamesstimacdec@gmail.com>
2024-08-25 12:34:58 -0400

    The strings are not saving even though they are reading properly. Either .push_back is the issue or separating the files has made the memory get lost from main.cpp even though that's where the string vector starts. -james

--- main.cpp
+++ main.cpp
@@ -2,8 +2,9 @@
 
 int main()
 {
-	// vector string to store input
+	// vector string
 	std::vector<std::string> testInput;
+	std::vector<char> parsedStrings;
 	
 	// editable filename
 	std::string fileName = "TestInput.txt";
@@ -11,7 +12,22 @@ int main()
 	// read file
 	Trebuchet::readFile(testInput, fileName);
 
-
+	// 
+	std::cout << std::endl << std::endl;
+	//
+ 
+	// FIXME: doesn't save input to testInput
+	// start: read individual chars
+	// iterate over testInput string vector
+	for (int i = 0; i < testInput.size(); i++)
+	{
+		// iterate over individual string length
+		for (int j = 0; j < testInput.at(i).size(); j++)
+		{
+			std::cout << "saved char from " << i << ". in string at " << j << ": " << testInput.at(i).at(j) << std::endl;
+			parsedStrings.push_back(testInput.at(i).at(j));
+		}
+	}
 
 	std::cin.get();
 	return 0;
